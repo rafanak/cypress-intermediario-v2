6d5791f897dc3afab3ef8b39c540606ff4acac3a
Rafaela Nakashima <rafaela.naka@gmail.com>
2023-10-13 20:45:04 -0300

    Aula 12

--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -1,3 +1,4 @@
+//Login
 Cypress.Commands.add('login', (
     user = Cypress.env('user_name'),
     password = Cypress.env('user_password'),
@@ -13,7 +14,18 @@ Cypress.Commands.add('login', (
     login()
   })
   
+ //Logout 
 Cypress.Commands.add('logout', () => {
     cy.get('.qa-user-avatar').click()
     cy.contains('Sign out').click()
+  })
+
+  //Criar projeto novo
+  Cypress.Commands.add('gui_createProject', project => {
+    cy.visit('/projects/new')
+  
+    cy.get('#project_name').type(project.name)
+    cy.get('#project_description').type(project.description)
+    cy.get('.qa-initialize-with-readme-checkbox').check()
+    cy.contains('Create project').click()
   })
